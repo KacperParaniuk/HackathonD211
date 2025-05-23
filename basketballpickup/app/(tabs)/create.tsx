1c0e8e4dd0690bc13ceb3ac48fd18a60bf32538b
PorebskiCode18 <damianfc37@gmail.com>
2025-05-22 20:10:28 -0500

    Update create.tsx

--- basketballpickup/app/(tabs)/create.tsx
+++ basketballpickup/app/(tabs)/create.tsx
@@ -378,7 +378,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 10,
+    marginBottom: 5,
   },
   
   pickerWrapper: {
